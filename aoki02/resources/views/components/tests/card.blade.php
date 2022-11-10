fe049642ac87da567f8a9f910bbb408ab5e81b28
なかおなるき <naruki@nakaonarukinoMacBook-Air-3.local>
2022-11-10 18:15:30 +0900

    props

--- aoki02/resources/views/components/tests/card.blade.php
+++ aoki02/resources/views/components/tests/card.blade.php
@@ -1,3 +1,5 @@
+@props(['title','message' => 'null', 'comment'=> 'null'])
+
 <div class="border-2 shadow-md w-1/4 p-2">
     <div>{{$title}}</div>
     <div>画像</div>
--- aoki02/resources/views/tests/component-test1.blade.php
+++ aoki02/resources/views/tests/component-test1.blade.php
@@ -4,5 +4,6 @@
 
 
 <x-tests.card title="タイトルー" comment="ほんぶんー" :message="$message"/>
+<x-tests.card title="タイトルー22" />
 
 </x-tests.app>
