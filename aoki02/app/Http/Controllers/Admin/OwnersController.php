c912410df0aba41fcf1fa925f4ea8e69a0af8635
なかおなるき <naruki@nakaonarukinoMacBook-Air-3.local>
2022-11-16 17:51:32 +0900

    form作成

--- aoki02/app/Http/Controllers/Admin/OwnersController.php
+++ aoki02/app/Http/Controllers/Admin/OwnersController.php
@@ -27,28 +27,32 @@ class OwnersController extends Controller
     public function index()
     {
 
-        $date_now=Carbon::now();
-        $date_parse=Carbon::parse(now());
+        // $date_now=Carbon::now();
+        // $date_parse=Carbon::parse(now());
 
-        echo $date_now->year;
-        echo '<br>';
-        echo $date_parse;
+        // echo $date_now->year;
+        // echo '<br>';
+        // echo $date_parse;
 
 
-        echo '<br>';
+        // echo '<br>';
 
-        $e_all = Owner::all();
-        $q_get = DB::table('owners')->select('name','created_at')->get();
-        // $q_first = DB::table('owners')->select('name')->first();
+        // $e_all = Owner::all();
+        // $q_get = DB::table('owners')->select('name','created_at')->get();
+        // // $q_first = DB::table('owners')->select('name')->first();
 
-        // $c_test = collect([
-        //     'name' => 'テスト'
+        // // $c_test = collect([
+        // //     'name' => 'テスト'
         // ]);
 
         // var_dump($q_first);
 
         // dd($e_all, $q_get, $q_first, $c_test);
-        return view('admin.owners.index', compact('e_all','q_get'));
+
+
+        $owners = Owner::select('name', 'email', 'created_at')-> get();
+
+        return view('admin.owners.index', compact('owners'));
     }
 
     /**
@@ -58,7 +62,7 @@ class OwnersController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.owners.create');
     }
 
     /**
--- aoki02/resources/views/admin/owners/index.blade.php
+++ aoki02/resources/views/admin/owners/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            オーナー一覧
         </h2>
     </x-slot>
 
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-
+{{--}}
                 エロクアント
                 @foreach($e_all as $e_owner)
                 {{$e_owner -> name}}
@@ -24,6 +24,60 @@
                 {{ Carbon\Carbon::parse($q_owner -> created_at) -> diffForHumans()}}
 
                 @endforeach
+
+--}}
+
+        <section class="text-gray-400  body-font">
+                  <div class="container px-5 py-4 mx-auto">
+
+                    <div class="lg:w-2/3 w-full mx-auto overflow-auto">
+                      <table class="table-auto w-full text-left whitespace-no-wrap">
+                        <thead>
+                          <tr>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-white text-sm bg-gray-400 rounded-tl rounded-bl">Name</th>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-white text-sm bg-gray-400">Mail</th>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-white text-sm bg-gray-400">Created at</th>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-white text-sm bg-gray-400">Price</th>
+
+                          </tr>
+                        </thead>
+                        <tbody>
+
+                            @foreach($owners as $owner)
+
+
+
+
+
+
+                          <tr>
+                            <td class="px-4 py-3">{{$owner -> name}}</td>
+                            <td class="px-4 py-3">{{$owner -> email}}</td>
+                            <td class="px-4 py-3">{{$owner -> created_at -> diffForHumans()}}</td>
+                            <td class="w-10 text-center">
+                              <input name="plan" type="radio">
+                            </td>
+                          </tr>
+                          @endforeach
+
+                        </tbody>
+                      </table>
+                    </div>
+
+                    <div class=" flex justify-center flex-row items-center">
+
+                        <button onclick="location.href='{{route('admin.owners.index')}}'" class="border-4 border-gray-400 block w-[0%] md:w-[20%] text-center border bg-white rounded-full py-1 mt-6 text-lg font-bold tracking-wide uppercase text-black brightness-105 hover:bg-gray-400 basis-1/4 ">Reload</button>
+
+                        <button onclick="location.href='{{route('admin.owners.create')}}'"
+                            class="border-4 border-gray-400 block w-[30%] md:w-[20%] text-center border bg-white rounded-full py-1 mt-6 text-lg font-bold tracking-wide uppercase text-black brightness-105 hover:bg-gray-400 basis-1/4 ">Register</button>
+                    </div>
+
+
+
+                  </div>
+        </section>
+
+
 
 
                 </div>
